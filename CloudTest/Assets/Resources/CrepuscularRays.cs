4bb125dbdfab1823ebd8e54945a9d754ffbce69a
面向搜索引擎编程 <mapno1.com@gmail.com>
2020-12-23 10:36:19 +0800

    Update God Ray
    Update God Ray

---
+++ CloudTest/Assets/Resources/CrepuscularRays.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace UnityEngine.Rendering.PostProcessing
+{
+
+    public enum QualityMode
+    {
+        defaultQuality,
+        customQuality
+    }
+
+
+    [Serializable]
+    public sealed class CrepuscularRaysParameter : ParameterOverride<QualityMode> { }
+
+    [Serializable]
+    [PostProcess(typeof(CrepuscularRaysRender), PostProcessEvent.AfterStack, "Custom/CrepuscularRays")]
+    public sealed class CrepuscularRays : PostProcessEffectSettings
+    {
+
+        public CrepuscularRaysParameter QualitySetting = new CrepuscularRaysParameter { value = QualityMode.defaultQuality };
+
+        public ColorParameter LightColour = new ColorParameter { value = new Color(1.33f, 0.98f, 0.69f, 1) };
+        [Range(0, 2)]
+        public FloatParameter RayRange = new FloatParameter { value = 0.94f };
+        public FloatParameter RayIntensity = new FloatParameter { value = 2 };
+        [Range(1, 3)]
+        public FloatParameter RayPower = new FloatParameter { value = 1.25f };
+        [Range(0, 1)]
+        public FloatParameter LightThreshold = new FloatParameter { value = 0.29f };
+
+
+        [Range(2, 64)]
+        public FloatParameter qualityStep = new FloatParameter { value = 32 };
+        [Range(0, 0.1f)]
+        public FloatParameter OffsetUV = new FloatParameter { value = 0.027f };
+        [Range(0, 0.01f)]
+        public FloatParameter BoxBlur = new FloatParameter { value = 0.00126f };
+        [Range(1, 16)]
+        public IntParameter Downsample = new IntParameter { value = 4 };
+    }
+}
+
+public sealed class CrepuscularRaysRender : PostProcessEffectRenderer<CrepuscularRays>
+{
+
+    private GameObject LightObject = null;
+    private Light componetLight = null;
+
+    public override void Init()
+    {
+        LightObject = GameObject.Find("Directional Light");
+        componetLight = LightObject.GetComponent<Light>();
+    }
+    public override void Release()
+    {
+        if (componetLight)
+        {
+            LightObject = null;
+            componetLight = null;
+        }
+    }
+
+    public override void Render(PostProcessRenderContext context)
+    {
+        var cmd = context.command;
+        cmd.BeginSample("CrepuscularRays");
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/CrepuscularRays"));
+
+        Matrix4x4 projectionMatrix = GL.GetGPUProjectionMatrix(context.camera.projectionMatrix, false);
+        sheet.properties.SetMatrix(Shader.PropertyToID("_InverseProjectionMatrix"), projectionMatrix.inverse);
+        sheet.properties.SetMatrix(Shader.PropertyToID("_InverseViewMatrix"), context.camera.worldToCameraMatrix.inverse);
+
+        sheet.properties.SetFloat(Shader.PropertyToID("_RayRange"), settings.RayRange);
+        sheet.properties.SetFloat(Shader.PropertyToID("_RayIntensity"), settings.RayIntensity);
+        sheet.properties.SetFloat(Shader.PropertyToID("_RayPower"), settings.RayPower);
+        sheet.properties.SetFloat(Shader.PropertyToID("_LightThreshold"), settings.LightThreshold);
+        sheet.properties.SetFloat(Shader.PropertyToID("qualityStep"), settings.qualityStep);
+        sheet.properties.SetFloat(Shader.PropertyToID("OffsetUV"), settings.OffsetUV);
+        sheet.properties.SetFloat(Shader.PropertyToID("_BoxBlur"), settings.BoxBlur.value);
+        sheet.properties.SetColor(Shader.PropertyToID("LightColor"),settings.LightColour);
+        if (LightObject)
+        {
+            //取摄像机位置，只取灯光旋转方向
+            Vector3 LightPos = context.camera.WorldToViewportPoint(context.camera.transform.position + LightObject.transform.forward * context.camera.farClipPlane);
+            Vector4 viewPortLightPos = new Vector4(LightPos.x, LightPos.y, 0, 0);
+            sheet.properties.SetVector(Shader.PropertyToID("_LightViewPos"), viewPortLightPos);
+            sheet.properties.SetVector(Shader.PropertyToID("_LightDir"), LightObject.transform.forward);
+            sheet.properties.SetVector(Shader.PropertyToID("_CamDir"), context.camera.transform.forward);
+        }
+
+        var DownsampleTempID = Shader.PropertyToID("_DownsampleTemp");
+
+        context.GetScreenSpaceTemporaryRT(cmd, DownsampleTempID, 0,context.sourceFormat, RenderTextureReadWrite.Default, FilterMode.Bilinear, context.screenWidth/ settings.Downsample.value, context.screenHeight/ settings.Downsample.value);
+        cmd.BlitFullscreenTriangle(context.source, DownsampleTempID, sheet,1);
+        
+        cmd.SetGlobalTexture(Shader.PropertyToID("_LowResTexture"), DownsampleTempID);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+
+        cmd.ReleaseTemporaryRT(DownsampleTempID);
+        cmd.EndSample("CrepuscularRays");
+    }
+
+}
---
+++ CloudTest/Assets/Resources/CrepuscularRays.shader
@@ -0,0 +1,134 @@
+﻿Shader "Hidden/Custom/CrepuscularRays" 
+{		  
+		HLSLINCLUDE
+
+		#pragma target 3.0
+		#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+		float4 _LightViewPos;
+		float3 _LightDir;
+		float3 _CamDir;
+
+		float _RayRange;
+		float _RayIntensity;
+		float _RayPower;
+		float _DepthThreshold;
+				
+		float _LightThreshold;
+
+		float3 ShadowedFogColour;
+		float3 LightColor;
+				
+		float3 TerrainSize;
+		float qualityStep;
+		float OffsetUV;
+		float _BoxBlur;
+
+		TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+		TEXTURE2D_SAMPLER2D(_LowResTexture, sampler_LowResTexture);
+		float4 _MainTex_TexelSize;
+
+		//盒装模糊
+		half4 DownsampleBox4Tap(TEXTURE2D_ARGS(tex, samplerTex), float2 uv, float2 texelSize)
+		{
+			float4 d = texelSize.xyxy * float4(-1.0, -1.0, 1.0, 1.0);
+
+			half4 s;
+			s = (SAMPLE_TEXTURE2D(tex, samplerTex, UnityStereoTransformScreenSpaceTex(uv + d.xy)));
+			s += (SAMPLE_TEXTURE2D(tex, samplerTex, UnityStereoTransformScreenSpaceTex(uv + d.zy)));
+			s += (SAMPLE_TEXTURE2D(tex, samplerTex, UnityStereoTransformScreenSpaceTex(uv + d.xw)));
+			s += (SAMPLE_TEXTURE2D(tex, samplerTex, UnityStereoTransformScreenSpaceTex(uv + d.zw)));
+
+			return s * (1.0 / 4.0);
+		}
+
+		half4 FragDownsample4(VaryingsDefault i) : SV_Target
+		{
+			half4 color = DownsampleBox4Tap(TEXTURE2D_PARAM(_MainTex, sampler_MainTex), i.texcoord, _BoxBlur.xx);
+			return color;
+		}
+
+		half4 FragUpsample(VaryingsDefault i) : SV_Target
+		{
+			const float2 lowResTexelSize = 2.0 * _MainTex_TexelSize.xy;
+			float2 UV = i.texcoord - 0.5 * lowResTexelSize;
+			half4 color = SAMPLE_TEXTURE2D(_LowResTexture, sampler_LowResTexture, UnityStereoTransformScreenSpaceTex(UV));
+			return color;
+		}
+
+
+		float2 random(float2 p) {  //给UV 一个噪音
+			float a = dot(p, float2(114.5, 141.9));
+			float b = dot(p, float2(364.3, 648.8));
+			float2 c = sin(float2(a, b)) * 643.1;
+			return frac(c);
+		}
+
+		float4 Frag(VaryingsDefault i ):SV_Target
+		{				
+			float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex,i.texcoord);
+
+			//背面消隐
+			float backDir = saturate(dot(-_LightDir,_CamDir));
+
+			float2 ScreenUv = _LightViewPos.xy -  i.texcoord; //模糊向量
+			float lightViewDir = length(ScreenUv);
+			float distanceControl = saturate(_RayRange - lightViewDir);
+
+			float3 colorFinal = float3(0,0,0); 
+			float2 originalUV = i.texcoord;
+
+			float2 ScrUV = ScreenUv * OffsetUV;
+			float2 jitter = random(i.texcoord);
+
+			for (int ray = 0;ray<qualityStep; ray++)
+			{
+				float3 Addcolor = SAMPLE_TEXTURE2D(_LowResTexture, sampler_LowResTexture,originalUV + jitter * 0.005f).rgb;
+				float3 thresholdColor = saturate(Addcolor - _LightThreshold) * distanceControl;
+				float luminanceColor = dot(thresholdColor,float3(0.3f,0.59f,0.11f));
+				luminanceColor = pow(luminanceColor, _RayPower);
+				//luminanceColor *= sign(saturate(lindepth - _DepthThreshold));
+				colorFinal += luminanceColor;
+				originalUV += ScrUV ;
+			}
+			colorFinal = (colorFinal/qualityStep) * LightColor.rgb * _RayIntensity;
+					
+			return float4(colorFinal * backDir,1) + color;
+		}
+		ENDHLSL
+		  
+
+
+		SubShader
+		{
+			Cull Off ZWrite Off ZTest Always
+
+			Pass
+			{
+				HLSLPROGRAM
+
+				#pragma vertex VertDefault
+				#pragma fragment Frag
+
+				ENDHLSL
+			}
+			Pass
+			{
+				HLSLPROGRAM
+
+				#pragma vertex VertDefault
+				#pragma fragment FragDownsample4
+
+				ENDHLSL
+			}
+			Pass
+			{
+				HLSLPROGRAM
+
+				#pragma vertex VertDefault
+				#pragma fragment FragUpsample
+
+				ENDHLSL
+			}
+		 }
+
+}
